b13e6843be8cdbce4ba75c0f891cb09068074d6d
tendsT0Zero <nayeemofficial80@gmail.com>
2026-06-06 10:32:36 +0600

    feat:login  ui

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,7 +1,10 @@
-import React from "react";
-
-const page = () => {
-  return <div>Login</div>;
-};
-
-export default page;
+export default function LoginPage() {
+  return (
+    <div className="w-full flex flex-col items-center">
+      {/* form area */}
+      <div className="w-full p-4 border-2 border-dashed border-gray-300 rounded-lg text-center text-gray-400">
+        Login Form Will Be Placed Here
+      </div>
+    </div>
+  );
+}
