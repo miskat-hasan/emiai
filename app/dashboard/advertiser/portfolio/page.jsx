a9f6fa2ff6beb6b04e932b0894d6ddbd82388fa7
Miskat Hasan <miskathasan979@gmail.com>
2026-06-07 12:38:52 +0600

    fixed: import issues

--- app/dashboard/advertiser/portfolio/page.jsx
+++ app/dashboard/advertiser/portfolio/page.jsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { useMemo, useState } from "react";
-import PortfolioCard from "../../influencer/portfolio/components/PortfolioCard";
-import AddPortfolioModal from "../../influencer/portfolio/components/AddPortfolioModal";
-import PortfolioDetailsModal from "../../influencer/portfolio/components/PortfolioDetailsModal";
-import AgencyPortfolioDetailsModal from "../../influencer/portfolio/components/AgencyPortfolioDetailsModal";
+import PortfolioCard from "@/app/dashboard/influencer/portfolio/components/PortfolioCard";
+import AddPortfolioModal from "@/app/dashboard/influencer/portfolio/components/AddPortfolioModal";
+import PortfolioDetailsModal from "@/app/dashboard/influencer/portfolio/components/PortfolioDetailsModal";
+import AgencyPortfolioDetailsModal from "@/app/dashboard/influencer/portfolio/components/AgencyPortfolioDetailsModal";
 
 export const portfolioTabs = [
   { label: "My Portfolio", value: "my_portfolio" },
