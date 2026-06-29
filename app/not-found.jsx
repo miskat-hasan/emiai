01d49d7c796d03567a0a85e395208deed0f1dd89
Miskat Hasan <miskathasan979@gmail.com>
2026-06-29 09:06:27 +0600

    fixed: agency dashboard redirect issue

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,11 +1,16 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import React from "react";
 
-const page = () => {
+const Page = () => {
+  const router = useRouter();
   return (
-    <div className="flex justify-center items-center h-screen text-2xl">
+    <div className="flex flex-col gap-3 justify-center items-center h-screen text-2xl">
       Not Found
+      <button onClick={() => router.back()} className="bg-primary border border-secondary px-3 py-2 text-sm rounded-md cursor-pointer">Go Back</button>
     </div>
   );
 };
 
-export default page;
+export default Page;
